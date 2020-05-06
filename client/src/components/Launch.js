868f8120c8ecdcee866452fa27c16fbf40a9ad29
Oliver Sun <olisun@mac.com>
2020-05-06 12:46:38 -0700

    added code to display data in launch.js

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -3,6 +3,8 @@ import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import { Button, Card, Container, Col, Row } from "react-bootstrap";
+
 
 const LAUNCH_QUERY = gql`
   query LaunchOuery($flight_number: Int!) {
@@ -27,6 +29,7 @@ export class Launch extends Component {
     let { flight_number } = this.props.match.params;
     // Making sure the browser reads the flight number as a number.
     flight_number = parseInt(flight_number);
+
     return (
       <Fragment>
         <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
@@ -37,11 +40,22 @@ export class Launch extends Component {
               if (error) console.log(error)
 
               console.log(data)
-              return <Fragment>
-                {
-                  <h1>Test</h1>
-                }
-              </Fragment>
+              const {
+                mission_name,
+                launch_year,
+                launch_success,
+                launch_data_local,
+                rocket: { rocket_id, rocket_name, rocket_type }
+              } = data.launch
+              return <div>
+                <h4>{mission_name}</h4>
+                <h4>{launch_year}</h4>
+                <h4>{launch_success}</h4>
+                <h4>{launch_data_local}</h4>
+                <h4>{rocket_id}</h4>
+                <h4>{rocket_name}</h4>
+                <h4>{rocket_type}</h4>
+              </div>
             }
           }
         </Query>
